e853da9fe7cbabf25dfd10bcd99e2f61f8486cf3
putralangkat97 <pucukpisang97@gmail.com>
2019-10-08 19:52:37 +0700

    commit kesepuluh

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// PERHITUNGAN DANA PENDIDIKAN
+
 // perhitungan usia
 function hitungJumlah() {
     var umur    = document.getElementById('usia').value;
@@ -19,7 +21,7 @@ function hitungBiaya() {
     var jangka  = document.getElementById('jangka').value;
     var hitung  = Math.pow(1 + inflasi, jangka);
     var hitung2 = Math.floor(biaya * hitung);
-    var formatU = formatUang(hitung2);
+    var formatU = add_commas(hitung2);
     document.getElementById('biaya_nanti').innerHTML = "Rp. "+formatU;
 
     // hitungan kedua
@@ -32,10 +34,34 @@ function hitungBiaya() {
     var hasil_akhir     = Math.pow(hasil2, akhir);
     var hasil_akhir_2   = hasil_akhir - 1;
     var hasil_final     = Math.floor(hasil1/hasil_akhir_2);
-    var formatU2        = formatUang(hasil_final);
+    var formatU2        = add_commas(hasil_final);
     document.getElementById('tabungan_bulanan').innerHTML = "Rp. "+formatU2;
 }
 
-$(document).ready(function(){
-    $("#card1").css("center-block")   
-});
+// PERHITUNGAN PROTEKSI JIWA
+
+// hitung kewajiban
+function hitungKewajiban() {
+    var kpr         = parseInt(document.getElementById('saldo_kpr').value);
+    var kpm         = parseInt(document.getElementById('saldo_kpm').value);
+    var kk          = parseInt(document.getElementById('saldo_kk').value);
+    var kb          = parseInt(document.getElementById('saldo_kb').value);
+    var kl          = parseInt(document.getElementById('saldo_kl').value);
+    var hitungan    = kpr + kpm + kk + kb + kl;
+    var hitunganA   = add_commas(hitungan);
+    document.getElementById('total').innerHTML = "Rp. "+hitunganA;
+
+    // hitungan UpIdeal
+    var tahun       = parseInt(document.getElementById('penghasilan_tahun').value);
+    var bulan       = document.getElementById('penghasilan_bulan').value;
+    var asumsi      = document.getElementById('asumsi_bunga').value;
+    asumsi          = asumsi/100;
+    var upideal     = ((tahun/asumsi) + hitungan);
+    var hitunganB   = add_commas(upideal)
+    document.getElementById('up_ideal').innerHTML = "Rp. "+hitunganB;
+
+    // hitung UpMinimal
+    var upminimal   = ((bulan * 12)/asumsi + hitungan);
+    var hitunganC   = add_commas(upminimal);
+    document.getElementById('up_minimal').innerHTML = "Rp. "+hitunganC;
+}

7d237cfd4f81e42dda84ea25106e8fb163b58243
putralangkat97 <pucukpisang97@gmail.com>
2019-10-08 23:47:36 +0700

    commit ke-15

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,3 +65,64 @@ function hitungKewajiban() {
     var hitunganC   = add_commas(upminimal);
     document.getElementById('up_minimal').innerHTML = "Rp. "+hitunganC;
 }
+
+
+// Hitung Dana Pensiun
+function hitungPensiun() {
+    
+    var pensiun         = document.getElementById('dana_pensiun').value;
+    var tingkatBunga    = document.getElementById('tingkat_bunga').value;
+    tingkatBunga        = tingkatBunga/100;
+    var jumlah_ideal    = Math.floor((pensiun * 12) / tingkatBunga);
+    var hitunganD       = add_commas(jumlah_ideal);
+    document.getElementById('jumlah_ideal').innerHTML = "Rp. "+hitunganD;
+
+    // hitung usia
+    var usiaSekarang    = document.getElementById('usia_sekarang').value;
+    var usiaPensiun     = document.getElementById('usia_pensiun').value;
+    var jangkaWaktu     = usiaPensiun - usiaSekarang;
+    if(jangkaWaktu < 0) {
+        document.getElementById('jangka_waktu').value = "Anda Sudah Pensiun";
+    } else {
+        document.getElementById('jangka_waktu').value = jangkaWaktu;
+    }
+
+    var asumsi          = document.getElementById('asumsi_investasi').value;
+    asumsi              = asumsi/100;
+    var diskonto2       = 1/(1 + (asumsi/12));
+
+    // tabungan regular
+    var jumlahIngin     = document.getElementById('jumlah_keinginan').value;
+    var hitungan1       = jumlahIngin * ((asumsi/12) * diskonto2);
+    var hitungan2       = (1+(asumsi/12));
+    var akhir1          = jangkaWaktu * 12;
+    var hasilPangkat    = Math.pow(hitungan2, akhir1);
+    var hasilPangkat2   = hasilPangkat - 1;
+    var hasilFinal2     = Math.floor(hitungan1/hasilPangkat2);
+    var formatU3        = add_commas(hasilFinal2);
+    document.getElementById('tabungan_regular').innerHTML = "Rp. "+formatU3;
+}
+
+
+    
+
+// var biaya   = document.getElementById('biaya').value;
+// var inflasi = document.getElementById('inflasi').value;
+// inflasi     = inflasi/100;
+// var jangka  = document.getElementById('jangka').value;
+// var hitung  = Math.pow(1 + inflasi, jangka);
+// var hitung2 = Math.floor(biaya * hitung);
+// var formatU = add_commas(hitung2);
+// document.getElementById('biaya_nanti').innerHTML = "Rp. "+formatU;
+
+// var asumsi          = document.getElementById('asumsi').value;
+// asumsi              = asumsi/100;
+// var diskonto        = 1/(1+(asumsi/12));
+// var hasil1          = (hitung2*(asumsi/12*diskonto));
+// var akhir           = jangka*12;
+// var hasil2          = ((1 + asumsi/12));
+// var hasil_akhir     = Math.pow(hasil2, akhir);
+// var hasil_akhir_2   = hasil_akhir - 1;
+// var hasil_final     = Math.floor(hasil1/hasil_akhir_2);
+// var formatU2        = add_commas(hasil_final);
+// document.getElementById('tabungan_bulanan').innerHTML = "Rp. "+formatU2;
